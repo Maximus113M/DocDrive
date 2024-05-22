1e2f29762229f58fd5e11d4a53c613f86d2f6b10
Maximus113M <camilomantillaq13@gmail.com>
2024-05-21 21:53:41 -0500

    db migrations done

---
+++ database/migrations/2024_05_15_201012_create_folders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("documentPath");
+            $table->string("description");
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->unsignedBigInteger('visualization_role_id');
+            $table->foreign('visualization_role_id')->references('id')->on('visualization_roles');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folders');
+    }
+}
---
+++ database/migrations/2024_05_15_202748_create_documents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("documentPath");
+            $table->string("description");
+            $table->string("format");
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->unsignedBigInteger('folder_id')->nullable();
+            $table->foreign('folder_id')->references('id')->on('folders');
+            $table->unsignedBigInteger('visualization_role_id');
+            $table->foreign('visualization_role_id')->references('id')->on('visualization_roles');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
